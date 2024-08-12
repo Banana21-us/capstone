fa44a474f5d4af9c5f2542754623d70298ac1ecc
bigbanana <60498922+Banana21-us@users.noreply.github.com>
2024-08-13 01:28:16 +0800

    Update app routes and custom sidenav; Add new modules, login component, and assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,40 +6,26 @@ import { teacherroutes } from './Modules/Teacher/teacher.routes';
 import { announcementroutes } from './Modules/Announcement/announcement.routes';
 import { messageroutes } from './Modules/Message/message.routes';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { accountroutes } from './Modules/Account/account.routes';
 
 
 
 export const routes: Routes = [
-    
-    {path: 'main-page', component: MainPageComponent,
-    children: [
-        {
-            path: 'homepage',
-            loadChildren: () => import('./Modules/Dashboard/dashboard.routes').then(r=>Dashboardroute)
-        },
-        {
-            path: 'classmanagement',
-            loadChildren: () => import('./Modules/ClassManagement/classmanagement.routes').then(r=>managementroute)
-        },
-        {
-            path: 'teacher',
-            loadChildren: () => import('./Modules/Teacher/teacher.routes').then(r=>teacherroutes)
-        },
-        {
-            path: 'announcement',
-            loadChildren: () => import('./Modules/Announcement/announcement.routes').then(r=>announcementroutes)
-        },
-        {
-            path: 'message',
-            loadChildren: () => import('./Modules/Message/message.routes').then(r=>messageroutes)
-        },
-        
+    { path: 'login', component: LoginComponent },
+    { path: 'main-page', component: MainPageComponent,
+      children: [
+        { path: 'homepage', loadChildren: () => import('./Modules/Dashboard/dashboard.routes').then(r => Dashboardroute) },
+        { path: 'classmanagement', loadChildren: () => import('./Modules/ClassManagement/classmanagement.routes').then(r => managementroute) },
+        { path: 'teacher', loadChildren: () => import('./Modules/Teacher/teacher.routes').then(r => teacherroutes) },
+        { path: 'announcement', loadChildren: () => import('./Modules/Announcement/announcement.routes').then(r => announcementroutes) },
+        { path: 'message', loadChildren: () => import('./Modules/Message/message.routes').then(r => messageroutes) },
+        { path: 'account', loadChildren: () => import('./Modules/Account/account.routes').then(r => accountroutes) },
 
-        {path: '', redirectTo: 'homepage', pathMatch: 'full'}
-        ]
+        { path: '', redirectTo: 'homepage', pathMatch: 'full' }
+      ],
     },
-    {path: '', redirectTo: 'main-page', pathMatch:'full'}
-
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/custom-sidenav/custom-sidenav.component.ts
+++ src/app/custom-sidenav/custom-sidenav.component.ts
@@ -54,12 +54,12 @@ export class CustomSidenavComponent {
     {
       icon: 'account_circle',
       label: 'Profile',
-      route: ''
+      route: 'account'
     },
     {
       icon: 'logout',
       label: 'Logout',
-      route: ''
+      route: '/login'
     }
   ]);
 
